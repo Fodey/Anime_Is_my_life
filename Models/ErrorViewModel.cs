7fee73b290a5749445ab7ccd075884d359f36656
Fodey <81467383+Fodey@users.noreply.github.com>
2021-08-09 17:02:40 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Anime_Is_my_life.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
